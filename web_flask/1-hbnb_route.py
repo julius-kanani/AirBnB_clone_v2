f2932e943669363f61ed5b76346921a042c766ca
julius-kanani <juliusm.kanani@gmail.com>
2024-07-29 01:59:17 +0300

    modified 1-hbnb_route

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-1-hello_route module that starts a simple flask web application.
+1-hbnb_route module that starts a simple flask web application.
 """
 
 
@@ -12,13 +12,13 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_route():
-    """ Display "Hello HBNB!". """
+    """ Display "Hello HBNB!. """
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
-def hello_route():
-    """ Route to display "HBNB". """
+def hbnb_route():
+    """ Route to display "HBNB. """
     return "HBNB"
 
 
